c13418257f9b3279cbe7508f2a01b95df97750c5
Pearlz123 <comfort.ajani@stu.cu.edu.ng>
2022-08-29 10:58:43 -0700

    Task zero

---
+++ Task_0.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main-prints an expression as it is
+ * Return: 0 if exited properly and non zero if otherwise
+ */
+
+int main(void)
+{
+	printf("Negassi and Comfort are completing the simple shell project,\n");
+	return (0);
+}
