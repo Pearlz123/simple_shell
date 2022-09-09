afd6ba7a0d6bb3d8f17113fbb1076559de347b61
King2H <neg2htt@gmail.com>
2022-09-09 05:20:02 +0000

    Execute updated

--- execute.c
+++ execute.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 /**
- * execute - execute a command with its entire path variables.
+ * execute - executes a command with its entire path variables.
  * @data: a pointer to the program's data
- * Return: If sucess returns zero, otherwise, return -1.
+ * Return: zero for success, otherwise, return -1.
  */
 
 int execute(data_of_program *data)
