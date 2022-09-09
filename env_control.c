7bf0707b6e15e981660880b769002b32a1d34023
King2H <neg2htt@gmail.com>
2022-09-09 05:15:54 +0000

    Environment control Updated

--- env_control.c
+++ env_control.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * env_get_key - gets the value of an environment variable
+ * env_get_key - get the value of an environment variable
  * @key: the environment variable of interest
  * @data: struct of the program's data
  * Return: a pointer to the value of the variable or NULL if it doesn't exist
